ecfc8f6e9cc497b77e4300d90390f8fd20429f03
liam stabeno <liamstabeno@gmail.com>
2017-02-20 15:10:16 -0800

    create the initial files for grocery list page

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(function(){
+  $("#form").submit(function(){
+
+    event.preventDefault();
+    var items = ["q1", "q2" ,"q3" ,"q4" ,"q5"]
+    var itemInputs = [];
+    items.forEach(function(item){
+      itemInputs.push($("input#" + item).val().toUpperCase());
+    });
+console.log(itemInputs);
+    itemInputs.sort();
+console.log(itemInputs);
+    itemInputs.forEach(function(list){
+      $("#list").append("<li>"+list+"</li>")
+    });
+
+
+});
+
+});
