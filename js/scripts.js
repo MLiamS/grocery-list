213b934038b5f76e95038893d4596442b6b2aada
Liam Stabeno <liamstabeno@gmail.com>
2017-02-21 09:31:35 -0800

    add a for loop in place of the forEach function, they do the same thing

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,13 @@ $(function(){
     event.preventDefault();
     var items = ["q1", "q2" ,"q3" ,"q4" ,"q5"]
     var itemInputs = [];
-    items.forEach(function(item){
-      itemInputs.push($("input#" + item).val().toUpperCase());
-    });
+    // items.forEach(function(item){
+    //   itemInputs.push($("input#" + item).val().toUpperCase());//(A) this and (B) do the same thing
+     // });
+
+    for(i=0;i<items.length;i++){
+      itemInputs.push($("input#" + items[i]).val().toUpperCase());//(B) this and (A) do the same thing
+    };
 console.log(itemInputs);
     itemInputs.sort();
 console.log(itemInputs);
